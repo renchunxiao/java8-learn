bfd25fc56edd2f6e4de623f4dac6219dcd666294
renchunxiao <rcx123tiancai@126.com>
2015-12-15 23:13:29 +0800

    添加 new 日期 api

--- src/main/java/com/rcx/java8/chapter5/TestMain.java
+++ src/main/java/com/rcx/java8/chapter5/TestMain.java
@@ -1,8 +1,17 @@
 package com.rcx.java8.chapter5;
 
+import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
 
 import org.junit.Test;
 
@@ -26,7 +35,7 @@ public class TestMain {
 		System.out.println(duration.plusMillis(500).toMillis());
 	}
 
-	@Test
+	// @Test
 	public void test2() {
 		// LocalDate.now() 获取本地今天日期
 		LocalDate localDate = LocalDate.now();
@@ -36,6 +45,67 @@ public class TestMain {
 		System.out.println(localDate2);
 		System.out.println(localDate2.plusDays(1));
 		System.out.println(localDate.isBefore(localDate2));
-
+		LocalDate localDate3 = localDate2.plus(Period.ofDays(3));
+		System.out.println(localDate.isBefore(localDate3));
+		System.out.println(localDate2.until(localDate3, ChronoUnit.DAYS));// 日期的距离
+		System.out.println(localDate3.getDayOfWeek());// 星期几
 	}
+
+	// @Test
+	public void test3() {
+		LocalDate localDate = LocalDate.of(2015, 1, 1).with(
+				TemporalAdjusters.nextOrSame(DayOfWeek.TUESDAY));// 从指定日期开始的后一个星期几
+		System.out.println(localDate);
+		LocalDate localDate2 = localDate.with(TemporalAdjusters.previous(DayOfWeek.TUESDAY));// 指定日期的前一个星期几
+		LocalDate localDate3 = localDate.with(TemporalAdjusters.next(DayOfWeek.TUESDAY));// 指定日期的后一个星期几
+		System.out.println(localDate2);
+		System.out.println(localDate3);
+		LocalDate localDate4 = localDate.with(TemporalAdjusters.dayOfWeekInMonth(3,
+				DayOfWeek.TUESDAY));// 第三个周二
+		System.out.println(localDate4);
+		LocalDate localDate5 = localDate.with(TemporalAdjusters.lastInMonth(DayOfWeek.TUESDAY));
+		System.out.println(localDate5);
+
+		TemporalAdjuster Next_WorkDay = w -> {// 自定义校正器
+			LocalDate resultDate = (LocalDate) w;
+			do {
+				resultDate = resultDate.plusDays(1);
+			} while (resultDate.getDayOfWeek().getValue() >= 6);
+			return resultDate;
+		};
+	}
+
+	// @Test
+	public void test4() {
+		LocalTime localTime = LocalTime.now();
+		LocalTime localTime2 = LocalTime.of(12, 24);
+		System.out.println(localTime);
+		System.out.println(localTime2);
+		// 其他 api 与 LocalDate 类似
+		LocalDateTime localDateTime = LocalDateTime.now();
+		LocalDateTime localDateTime2 = LocalDateTime.of(2015, 12, 22, 3, 14, 23);
+		System.out.println(localDateTime);
+		System.out.println(localDateTime2);
+		// 其他 api 与 LocalDate 类似
+	}
+
+	@Test
+	public void test5() {
+		LocalDateTime localDateTime = LocalDateTime.now();
+		System.out.println(DateTimeFormatter.ISO_DATE_TIME.format(localDateTime));// 预定义标准格式
+		System.out.println(DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(localDateTime));// 预定义标准格式
+
+		System.out.println(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)
+				.format(localDateTime));// 语言相关格式
+		System.out.println(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).format(
+				localDateTime));// 语言相关格式
+		System.out.println(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG).format(
+				localDateTime));// 语言相关格式
+		System.out.println(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).format(
+				localDateTime));// 语言相关格式，一般使用这个
+
+		System.out
+				.println(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(localDateTime));// 自定义
+	}
+
 }
